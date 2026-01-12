3b2c5fa5fe652bf0d4a883fb39c661bbd3da61b3
kishorikamali <kishorimagi159@gmail.com>
2026-01-12 21:37:46 +0530

    Initial commit - Java program

---
+++ DA1_23MIC0055/src/student/StudentInfo.java
@@ -0,0 +1,9 @@
+package student;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Kishori Kamali");
+        System.out.println("Register Number: 23MIC0055");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
